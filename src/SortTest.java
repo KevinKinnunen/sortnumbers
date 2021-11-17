d1d3c8e85a86ccd12e0fe7ab270d4683a41545fa
KevinKinnunen <54663592+KevinKinnunen@users.noreply.github.com>
2021-11-17 13:11:16 +0100

    Update SortTest.java

--- src/SortTest.java
+++ src/SortTest.java
@@ -1,7 +1,25 @@
-import java.util.*; //For Sort?
+//import java.util.Scanner;
+//import java.util.Arrays;
 
 public class SortTest {
-    public static void main(int[] args) {
-        // TODO
+    public static void main(String[] args) {
+        int[] arr1 = {2, 3, 1};
+        int[] arr2 = {300, 200, 100};
+        int[] arr3 = {-2, -1, -4};
+        int[] arr4 = {0, -5, -3};
+
+        testAdder(arr1);
+        testAdder(arr2);
+        testAdder(arr3);
+        testAdder(arr4);
+    }
+
+    private static void testAdder(int[] arr1) {
+        int[] res = Sorter.sort(arr1);
+        if (arr1 == res) {
+            System.out.println("OK");
+        } else {
+            System.err.println("Error: recieved " + res + ", expected " + arr1);
+        }
     }
 }
